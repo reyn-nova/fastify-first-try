36abbf884e4835cc7b7c9b4a2570a1c2bad65c8b
rynvva <rynvva@gmail.com>
2021-01-25 22:11:33 +0700

    0.0.5

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const fastify = require('fastify')
 
 const { root } = require('./endpoints-functions.js')
 
+const PORT = process.env.PORT || 3000
+const ADDRESS = '0.0.0.0'
+
 const fastifyInstance = fastify({
   logger: true
 })
 
 fastifyInstance.get('/', root)
 
-fastifyInstance.listen(process.env.PORT || 3000, (err) => {
+fastifyInstance.listen(PORT, ADDRESS, (err) => {
   if (err) {
     app.log.error(err)
     
